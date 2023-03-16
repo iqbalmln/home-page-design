5b4aed6e7aeb22712f8569c49b95abb2d5c8595b
iqbalmln <iqbalmaulana15784@gmail.com>
2023-03-16 23:01:53 +0700

    build: integrate @nuxtjs/tailwindcss module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      title: 'Landing Page | Tlab',
+    },
+  },
+  modules: ['@nuxtjs/tailwindcss']
 })
